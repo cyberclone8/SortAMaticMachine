63ff9f6f5466edd6488e14efc8fb2aaadfab087d
JM <magnayesxcz08@gmail.com>
2025-09-15 11:59:24 +0800

    FEAT: Fix the error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,11 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import Mainlayout from './layout/Mainlayout'
-import viteLogo from '/vite.svg'
-=======
 import AppRouter from "./routes/AppRoutes";
->>>>>>> 07056660920abd5b1e05ab10c2d225697d0307bb
 import './App.css'
-import { ClassNames } from "@emotion/react";
 
 function App() {
-<<<<<<< HEAD
-
-  return (
-    <Mainlayout>
-
-    </Mainlayout>
-=======
   return (
     <div>
       <AppRouter />;
     </div>
->>>>>>> 07056660920abd5b1e05ab10c2d225697d0307bb
   )
 }
 
